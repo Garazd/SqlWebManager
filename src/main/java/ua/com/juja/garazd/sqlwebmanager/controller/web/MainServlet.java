2943a13ed382c766cc4c45c81ffca6c99c981b49
Garazd <GarazdVZ@gmail.com>
2016-11-11 22:54:06 +0200

    Refactoring class added new command connect

--- src/main/java/ua/com/juja/garazd/sqlwebmanager/controller/web/MainServlet.java
+++ src/main/java/ua/com/juja/garazd/sqlwebmanager/controller/web/MainServlet.java
@@ -5,17 +5,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import ua.com.juja.garazd.sqlwebmanager.service.Service;
+import ua.com.juja.garazd.sqlwebmanager.service.ServiceImpl;
 
 public class MainServlet extends HttpServlet {
+
+    private Service service;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        service = new ServiceImpl();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = getAction(request);
 
-        if (action.equals("/menu")) {
+        if (action.startsWith("/menu") || action.equals("/")) {
+            request.setAttribute("items", service.commandsList());
             request.getRequestDispatcher("menu.jsp").forward(request, response);
-        } else if (action.equals("/help")) {
+        } else if (action.startsWith("/help")) {
             request.getRequestDispatcher("help.jsp").forward(request, response);
+        } else if (action.startsWith("/connect")) {
+            request.getRequestDispatcher("connect.jsp").forward(request, response);
         } else {
             request.getRequestDispatcher("error.jsp").forward(request, response);
         }
@@ -25,5 +39,23 @@ public class MainServlet extends HttpServlet {
         String requestURI = request.getRequestURI();
         String action = requestURI.substring(request.getContextPath().length(), requestURI.length());
         return action;
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = getAction(request);
+
+        if (action.startsWith("/connect")) {
+            String databaseName = request.getParameter("databaseName");
+            String userName = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            try {
+                service.connect(databaseName, userName, password);
+                response.sendRedirect(response.encodeRedirectURL("menu"));
+            } catch (Exception e) {
+                request.setAttribute("message", e.getMessage());
+                request.getRequestDispatcher("error.jsp").forward(request, response);
+            }
+        }
     }
 }
