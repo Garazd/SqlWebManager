d270f3dc62c267cbbad8567cd5d3fcea0bca9da1
Garazd <GarazdVZ@gmail.com>
2016-11-12 22:17:12 +0200

    Refactoring class added new command find

--- src/main/java/ua/com/juja/garazd/sqlwebmanager/controller/web/MainServlet.java
+++ src/main/java/ua/com/juja/garazd/sqlwebmanager/controller/web/MainServlet.java
@@ -5,8 +5,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import ua.com.juja.garazd.sqlwebmanager.model.DatabaseManager;
 import ua.com.juja.garazd.sqlwebmanager.service.Service;
-import ua.com.juja.garazd.sqlwebmanager.service.ServiceImpl;
+import ua.com.juja.garazd.sqlwebmanager.service.DatabaseService;
 
 public class MainServlet extends HttpServlet {
 
@@ -16,18 +17,23 @@ public class MainServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
 
-        service = new ServiceImpl();
+        service = new DatabaseService();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = getAction(request);
+        String tableName = "tableName";
+        DatabaseManager manager = (DatabaseManager) request.getSession().getAttribute("database_manager");
 
         if (action.startsWith("/menu") || action.equals("/")) {
             request.setAttribute("items", service.commandsList());
             request.getRequestDispatcher("menu.jsp").forward(request, response);
         } else if (action.startsWith("/help")) {
             request.getRequestDispatcher("help.jsp").forward(request, response);
+        } else if (action.startsWith("/find")) {
+            request.setAttribute("rows", service.getTableData(manager, tableName));
+            request.getRequestDispatcher("find.jsp").forward(request, response);
         } else if (action.startsWith("/connect")) {
             request.getRequestDispatcher("connect.jsp").forward(request, response);
         } else {
@@ -50,7 +56,8 @@ public class MainServlet extends HttpServlet {
             String password = request.getParameter("password");
 
             try {
-                service.connect(databaseName, userName, password);
+                DatabaseManager manager = service.connect(databaseName, userName, password);
+                request.getSession().setAttribute("database_manager", manager);
                 response.sendRedirect(response.encodeRedirectURL("menu"));
             } catch (Exception e) {
                 request.setAttribute("message", e.getMessage());
