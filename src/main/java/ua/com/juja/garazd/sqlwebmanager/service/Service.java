6f7500e3b7f9aa2587c18f9989c42117e6490481
Garazd <GarazdVZ@gmail.com>
2016-11-12 22:17:46 +0200

    Refactoring interface added new method getTableData

--- src/main/java/ua/com/juja/garazd/sqlwebmanager/service/Service.java
+++ src/main/java/ua/com/juja/garazd/sqlwebmanager/service/Service.java
@@ -1,10 +1,13 @@
 package ua.com.juja.garazd.sqlwebmanager.service;
 
 import java.util.List;
+import ua.com.juja.garazd.sqlwebmanager.model.DatabaseManager;
 
 public interface Service {
 
     List<String> commandsList();
 
-    void connect(String databaseName, String userName, String password);
+    DatabaseManager connect(String databaseName, String userName, String password);
+
+    List<List<String>> getTableData(DatabaseManager manager, String tableName);
 }
