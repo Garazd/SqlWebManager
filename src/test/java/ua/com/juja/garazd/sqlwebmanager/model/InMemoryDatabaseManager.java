6c896aa07d202fe4ac3725efa9a5f88528f1bd76
Garazd <GarazdVZ@gmail.com>
2016-11-26 23:44:41 +0200

    Added InMemoryDatabaseManager

---
+++ src/test/java/ua/com/juja/garazd/sqlwebmanager/model/InMemoryDatabaseManager.java
@@ -0,0 +1,91 @@
+package ua.com.juja.garazd.sqlwebmanager.model;
+
+import java.util.*;
+
+public class InMemoryDatabaseManager implements DatabaseManager {
+
+    private Map<String, Object> tables = new LinkedHashMap<>();
+
+    @Override
+    public List<Map<String, Object>> getTableData(String tableName) {
+        List<Map<String, Object>> result = new LinkedList<>();
+        return result;
+    }
+
+    @Override
+    public int getTableSize(String tableName) {
+        return get(tableName).size();
+    }
+
+    @Override
+    public Set<String> getTableNames() {
+        return tables.keySet();
+    }
+
+    @Override
+    public void connectDatabase(String database, String userName, String password) {
+        // do nothing
+    }
+
+    @Override
+    public void clearTable(String tableName) {
+        get(tableName).clear();
+    }
+
+    private List<Object> get(String tableName) {
+        if (!tables.containsKey(tableName)) {
+            tables.put(tableName, new LinkedList<>());
+        }
+        return (List<Object>) tables.get(tableName);
+    }
+
+    @Override
+    public void createEntry(String tableName, Map<String, Object> input) {
+        get(tableName).add(input);
+    }
+
+    @Override
+    public void updateTable(String tableName, int id, Map<String, Object> newValue) {
+        // do nothing
+    }
+
+    @Override
+    public Set<String> getTableColumns(String tableName) {
+        return new LinkedHashSet<>(Arrays.asList("name", "password", "id"));
+    }
+
+    @Override
+    public Set<String> getDatabasesName() {
+        return null;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return true;
+    }
+
+    @Override
+    public void disconnectDatabase() {
+        // do nothing
+    }
+
+    @Override
+    public void createDatabase(String databaseName) {
+        // do nothing
+    }
+
+    @Override
+    public void dropDatabase(String databaseName) {
+        // do nothing
+    }
+
+    @Override
+    public void createTable(String query) {
+        // do nothing
+    }
+
+    @Override
+    public void dropTable(String tableName) {
+        // do nothing
+    }
+}
