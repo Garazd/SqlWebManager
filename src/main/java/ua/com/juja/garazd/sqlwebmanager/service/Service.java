6f3ae8120cc76e44a2a4e99a521d960d171051f4
Garazd <GarazdVZ@gmail.com>
2016-11-28 21:45:55 +0200

    Added other method and refactoring connectDatabase

--- src/main/java/ua/com/juja/garazd/sqlwebmanager/service/Service.java
+++ src/main/java/ua/com/juja/garazd/sqlwebmanager/service/Service.java
@@ -1,13 +1,32 @@
 package ua.com.juja.garazd.sqlwebmanager.service;
 
 import java.util.List;
+import java.util.Set;
 import ua.com.juja.garazd.sqlwebmanager.model.DatabaseManager;
 
 public interface Service {
 
+    DatabaseManager connectDatabase(String databaseName, String userName, String password) throws ServiceException;
+
+    Set<String> tables(DatabaseManager manager);
+
+    void clearTable(DatabaseManager manager, String table);
+
+    void createDatabase(DatabaseManager manager, String database);
+
+    void createEntry(DatabaseManager manager, String table, String[] values);
+
+    void createTable(DatabaseManager manager, String query);
+
+    void disconnectDatabase(DatabaseManager manager);
+
+    void dropDatabase(DatabaseManager manager, String database);
+
+    void dropTable(DatabaseManager manager, String table);
+
     List<String> commandsList();
 
-    DatabaseManager connectDatabase(String databaseName, String userName, String password);
-
     List<List<String>> getTableData(DatabaseManager manager, String tableName);
+
+    void updateTable(DatabaseManager manager, String table, String[] values);
 }
