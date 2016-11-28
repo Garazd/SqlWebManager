07527ee301268a594e034ac4a915d736ce95bad6
Garazd <GarazdVZ@gmail.com>
2016-11-28 21:46:20 +0200

    Added implementation other method and refactoring connectDatabase

--- src/main/java/ua/com/juja/garazd/sqlwebmanager/service/DatabaseService.java
+++ src/main/java/ua/com/juja/garazd/sqlwebmanager/service/DatabaseService.java
@@ -2,19 +2,25 @@ package ua.com.juja.garazd.sqlwebmanager.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ua.com.juja.garazd.sqlwebmanager.model.DatabaseManager;
 import ua.com.juja.garazd.sqlwebmanager.model.PostgresDatabaseManager;
 
 @Component
-public class DatabaseService implements Service {
+public abstract class DatabaseService implements Service {
 
     @Autowired
     private PostgresDatabaseManager manager;
     private String className;
+    private String getDatabaseManager;
+
+    protected abstract DatabaseManager getDatabaseManager();
 
     @Override
     public List<String> commandsList() {
@@ -22,9 +28,56 @@ public class DatabaseService implements Service {
     }
 
     @Override
-    public DatabaseManager connectDatabase(String databaseName, String userName, String password) {
-        manager.connectDatabase(databaseName, userName, password);
-        return manager;
+    public DatabaseManager connectDatabase(String databaseName, String userName, String password) throws ServiceException {
+        try {
+            DatabaseManager manager = getDatabaseManager();
+            manager.connectDatabase(databaseName, userName, password);
+            return manager;
+        } catch (Exception e) {
+            throw new ServiceException("Connection error", e);
+        }
+    }
+
+    @Override
+    public void clearTable(DatabaseManager manager, String table) {
+        manager.clearTable(table);
+    }
+
+    @Override
+    public void createDatabase(DatabaseManager manager, String database) {
+        manager.createDatabase(database);
+    }
+
+    @Override
+    public void createEntry(DatabaseManager manager, String table, String[] values) {
+        Set<String> columns = manager.getTableColumns(table);
+
+        Map<String, Object> result = new LinkedHashMap<>();
+        Iterator<String> iterator = columns.iterator();
+        for (int index = 0; iterator.hasNext(); index++) {
+            result.put(iterator.next(), values[index]);
+        }
+        manager.createEntry(table, result);
+    }
+
+    @Override
+    public void createTable(DatabaseManager manager, String query) {
+        manager.createTable(query);
+    }
+
+    @Override
+    public void disconnectDatabase(DatabaseManager manager) {
+        manager.disconnectDatabase();
+    }
+
+    @Override
+    public void dropDatabase(DatabaseManager manager, String database) {
+        manager.dropDatabase(database);
+    }
+
+    @Override
+    public void dropTable(DatabaseManager manager, String table) {
+        manager.dropTable(table);
     }
 
     @Override
@@ -45,7 +98,26 @@ public class DatabaseService implements Service {
         return result;
     }
 
-    public void setClassName(String className) {
-        this.className = className;
+    @Override
+    public Set<String> tables(DatabaseManager manager) {
+        return manager.getTableNames();
+    }
+
+    @Override
+    public void updateTable(DatabaseManager manager, String table, String[] values) {
+        Map<String, String> updateValues = new LinkedHashMap<>();
+        int index = 0;
+        for (String column: manager.getTableColumns(table)) {
+            updateValues.put(column, values[index++]);
+        }
+        String primaryKeyName = manager.getPrimaryKey(table);
+        String primaryKeyValue = updateValues.remove(primaryKeyName);
+
+        manager.updateTable(table, primaryKeyName, primaryKeyValue, updateValues);
+            String.format("UPDATE RECORD IN '%s' TABLE", table);
+    }
+
+    public void setGetDatabaseManager(String getDatabaseManager) {
+        this.getDatabaseManager = getDatabaseManager;
     }
 }
